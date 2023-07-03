a56f723d43b8a816bfd40e2cf139423853d54346
Pavithra <pavithra.n@revature.com>
2023-07-03 11:35:54 +0530

    commit-3

--- src/main/java/AdvancedBooleanLogic.java
+++ src/main/java/AdvancedBooleanLogic.java
@@ -1,7 +1,7 @@
 /**
  * Boolean operators can be combined just like having multiple mathematical operators in a math equation.
  * Like math equations, they can be combined using parentheses, where the parentheses are completed first. Otherwise,
- * boolean operators are read left to right.edited
+ * boolean operators are read left to right.
  *
  * Write this boolean logic without using any if statements - that is, you should only use &&, ||, !, ==. All of these
  * problems can be solved in a single line.
